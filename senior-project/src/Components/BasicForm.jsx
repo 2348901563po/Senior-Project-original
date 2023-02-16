89fcd61eb8467107be712fcfeac85bf149c95020
2348901563po <72119878+2348901563po@users.noreply.github.com>
2023-02-16 10:26:06 -0600

    Started My Report Form

--- senior-project/src/Components/BasicForm.jsx
+++ senior-project/src/Components/BasicForm.jsx
@@ -1,1 +1,12 @@
 import * as React from 'react';
+
+const reportform = ()=>{
+    return(
+        <form>
+            <label htmlfor="name"></label>
+            <input for="name"></input>
+        </form>
+    )
+};
+
+export default BasicForm;
