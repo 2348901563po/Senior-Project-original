b15cdc8ccddc41ce6710731def98f75d15fbdf18
2348901563po <72119878+2348901563po@users.noreply.github.com>
2023-02-16 10:47:01 -0600

    Added on to the Login Page

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -9,39 +9,8 @@ import AdminSearch from './Components/AdminSearch';
 import BasicForm from './Components/BasicForm';
 import Adminhome from '/Components/Adminhome';
 import AdminSettings from '/Components/AdminSettings';
-const App = () => {
-  return (
-    <Login />
-  );
-};
+import Login from './Components/Login';
 
-const Login = () =>{
-  return(
-    <form>
-      <Username />
-      <Password />
-      <button>Submit</button>
-    </form>
-)};
 
-const Username = () =>{
-  return(
-  <div id='username'>
-    <label htmlFor='username'></label>
-    <input name='username' type='text'></input>
-    </div>
-  )};
 
-const Password = ()=>{
-  return(
-    <div id='password'>
-      <label htmlFor='password'></label>
-      <input name='password' type='password'></input>
-    </div>
-  )
-}
-
-function SubmitButton(){
-  const [] = useState();
-}
 export default App;
