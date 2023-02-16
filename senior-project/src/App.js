dc247bf8431c8bf932d59bc761c7afb2525200de
2348901563po <72119878+2348901563po@users.noreply.github.com>
2023-02-16 10:42:05 -0600

    Created a login page

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, 
+         Switch, 
+         Route, 
+         Redirect,} 
+         from "react-router-dom"; /*Retrieved from https://www.geeksforgeeks.org/how-to-redirect-to-another-page-in-reactjs/*/
+import AdminSearch from './Components/AdminSearch';
+import BasicForm from './Components/BasicForm';
+import Adminhome from '/Components/Adminhome';
+import AdminSettings from '/Components/AdminSettings';
 const App = () => {
   return (
     <Login />
