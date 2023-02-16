07f3159eb6014c84026b67136a24a67e50560cf1
2348901563po <72119878+2348901563po@users.noreply.github.com>
2023-02-16 10:07:08 -0600

    Added software packages to transition pages

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login />
   );
+};
+
+const Login = () =>{
+  return(
+    <form>
+      <Username />
+      <Password />
+      <button>Submit</button>
+    </form>
+)};
+
+const Username = () =>{
+  return(
+  <div id='username'>
+    <label htmlFor='username'></label>
+    <input name='username' type='text'></input>
+    </div>
+  )};
+
+const Password = ()=>{
+  return(
+    <div id='password'>
+      <label htmlFor='password'></label>
+      <input name='password' type='password'></input>
+    </div>
+  )
 }
 
+function SubmitButton(){
+  const [] = useState();
+}
 export default App;
